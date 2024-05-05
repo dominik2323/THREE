45a2abd19b827d7b5f71f78a2b2db6686e3d76f1
d91 <dominik.tomcik23@gmail.com>
2024-05-05 23:27:43 +0200

    mandelbulb

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ import fragment from "./shader/fragment.glsl";
 import vertex from "./shader/vertex.glsl";
 import * as dat from "dat.gui";
 
-
 import { TimelineMax } from "gsap";
 let OrbitControls = require("three-orbit-controls")(THREE);
+
+function map(n, fromStart, fromEnd, toStart, toEnd) {
+  return toStart + ((n - fromStart) / fromEnd - fromStart) * (toEnd - toStart);
+}
+
+function carthesianToSpherical(x, y, z) {
+  let r = Math.sqrt(x * x + y * y + z * z);
+  let theta = Math.atan2(Math.sqrt(x * x + y * y), z);
+  let phi = Math.atan2(y, x);
+  return [r, theta, phi];
+}
+
+function sphericalToCarthesian(r, theta, phi) {
+  let x = r * Math.sin(theta) * Math.cos(phi);
+  let y = r * Math.sin(theta) * Math.sin(phi);
+  let z = r * Math.cos(theta);
+  return [x, y, z];
+}
 
 export default class Sketch {
   constructor(selector) {
@@ -18,7 +35,7 @@ export default class Sketch {
     this.height = window.innerHeight;
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
-    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.setClearColor(0x000000, 1);
 
     this.container = document.getElementById("container");
     this.width = this.container.offsetWidth;
@@ -29,23 +46,20 @@ export default class Sketch {
       70,
       window.innerWidth / window.innerHeight,
       0.001,
-      1000
+      10000
     );
 
     // var frustumSize = 10;
     // var aspect = window.innerWidth / window.innerHeight;
     // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
-    this.camera.position.set(0, 0, 2);
+    this.camera.position.set(0, 0, 3);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.time = 0;
 
     this.paused = false;
 
-
-
     this.setupResize();
 
-    
     this.addObjects();
     this.resize();
     this.render();
@@ -70,24 +84,23 @@ export default class Sketch {
     this.height = this.container.offsetHeight;
     this.renderer.setSize(this.width, this.height);
     this.camera.aspect = this.width / this.height;
-    
 
     // image cover
-    this.imageAspect = 853/1280;
-    let a1; let a2;
-    if(this.height/this.width>this.imageAspect) {
-      a1 = (this.width/this.height) * this.imageAspect ;
+    this.imageAspect = 853 / 1280;
+    let a1;
+    let a2;
+    if (this.height / this.width > this.imageAspect) {
+      a1 = (this.width / this.height) * this.imageAspect;
       a2 = 1;
-    } else{
+    } else {
       a1 = 1;
-      a2 = (this.height/this.width) / this.imageAspect;
+      a2 = this.height / this.width / this.imageAspect;
     }
 
-    this.material.uniforms.resolution.value.x = this.width;
-    this.material.uniforms.resolution.value.y = this.height;
-    this.material.uniforms.resolution.value.z = a1;
-    this.material.uniforms.resolution.value.w = a2;
-
+    // this.material.uniforms.resolution.value.x = this.width;
+    // this.material.uniforms.resolution.value.y = this.height;
+    // this.material.uniforms.resolution.value.z = a1;
+    // this.material.uniforms.resolution.value.w = a2;
 
     // optional - cover with quad
     // const dist  = this.camera.position.z;
@@ -103,33 +116,95 @@ export default class Sketch {
     // }
 
     this.camera.updateProjectionMatrix();
-
-
   }
 
   addObjects() {
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
     let that = this;
-    this.material = new THREE.ShaderMaterial({
-      extensions: {
-        derivatives: "#extension GL_OES_standard_derivatives : enable"
-      },
-      side: THREE.DoubleSide,
-      uniforms: {
-        time: { type: "f", value: 0 },
-        resolution: { type: "v4", value: new THREE.Vector4() },
-        uvRate1: {
-          value: new THREE.Vector2(1, 1)
+    // this.material = new THREE.ShaderMaterial({
+    //   extensions: {
+    //     derivatives: "#extension GL_OES_standard_derivatives : enable",
+    //   },
+    //   side: THREE.DoubleSide,
+    //   uniforms: {
+    //     time: { type: "f", value: 0 },
+    //     resolution: { type: "v4", value: new THREE.Vector4() },
+    //     uvRate1: {
+    //       value: new THREE.Vector2(1, 1),
+    //     },
+    //   },
+    //   // wireframe: true,
+    //   // transparent: true,
+    //   vertexShader: vertex,
+    //   fragmentShader: fragment,
+    // });
+
+    // this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    // const sphereGeo = new THREE.SphereBufferGeometry(2.5, 64, 64);
+    // const spherePoints = sphereGeo.attributes.position.array;
+
+    // let transformedPoints = [];
+    // for (let i = 3; i <= spherePoints.length; i += 3) {
+
+    //   if (m < 10) {
+    //     transformedPoints.push(xe, ye, ze);
+    //   }
+    // }
+
+    let points = [];
+    let maxIterations = 50;
+    const length = 200;
+    for (let x = 0; x < length; x++) {
+      for (let y = 0; y < length; y++) {
+        let edge = false;
+        for (let z = 0; z < length; z++) {
+          const ux = map(x, 0, length, -1, 1);
+          const uy = map(y, 0, length, -1, 1);
+          const uz = map(z, 0, length, -1, 1);
+          let xe = 0;
+          let ye = 0;
+          let ze = 0;
+          let it = 0;
+          let n = 3;
+
+          while (true) {
+            const [r, t, p] = carthesianToSpherical(xe, ye, ze);
+            let m = Math.pow(r, n);
+            xe = ux + m * Math.sin(n * t) * Math.cos(n * p);
+            ye = uy + m * Math.sin(n * t) * Math.sin(n * p);
+            ze = uz + m * Math.cos(n * t);
+            it++;
+
+            if (r > 2) {
+              if (edge) {
+                edge = false;
+              }
+              break;
+            }
+            if (it > maxIterations) {
+              if (!edge) {
+                edge = true;
+                points.push(ux, uy, uz);
+              }
+              break;
+            }
+          }
         }
-      },
-      // wireframe: true,
-      // transparent: true,
-      vertexShader: vertex,
-      fragmentShader: fragment
+      }
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    this.geometry = geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(points), 3)
+    );
+    this.material = new THREE.PointsMaterial({
+      color: 0xffffff,
+      size: 0.005,
+      sizeAttenuation: true,
     });
-
-    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-
-    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.plane = new THREE.Points(this.geometry, this.material);
     this.scene.add(this.plane);
   }
 
@@ -145,7 +220,10 @@ export default class Sketch {
   render() {
     if (this.paused) return;
     this.time += 0.05;
-    this.material.uniforms.time.value = this.time;
+    // this.material.uniforms.time.value = this.time;
+    this.scene.rotateX(0.001);
+    this.scene.rotateY(0.001);
+    this.scene.rotateZ(0.001);
     requestAnimationFrame(this.render.bind(this));
     this.renderer.render(this.scene, this.camera);
   }
